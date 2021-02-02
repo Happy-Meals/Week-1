5b409d548993e3cfdb137bca691fc21dd440bd5d
BingChen Sun <sunbin@sheridan.desire2learn.com>
2021-02-01 20:36:24 -0500

    My First commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package studentlist;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author BingChen Sun
+ */
+public class Student {
+    private String studentName;
+    private String studentId;
+    private String program;
+    
+    public Student(String name, String id, String prog){
+        studentName=name;
+        studentId=id;
+        program=prog;
+    }
+
+    /**
+     * @return the studentName
+     */
+    public String getStudentName() {
+        return studentName;
+    }
+
+    /**
+     * @param studentName the studentName to set
+     */
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    /**
+     * @return the studentId
+     */
+    public String getStudentId() {
+        return studentId;
+    }
+
+    /**
+     * @param studentId the studentId to set
+     */
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    /**
+     * @return the program
+     */
+    public String getProgram() {
+        return program;
+    }
+
+    /**
+     * @param program the program to set
+     */
+    public void setProgram(String program) {
+        this.program = program;
+    }
+    
+}
