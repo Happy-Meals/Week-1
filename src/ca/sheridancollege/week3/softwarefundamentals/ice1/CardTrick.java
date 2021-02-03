019516adcbd4723feff7c6ef9c81c1d4e0a0712c
BingChen-Sun <77631070+Happy-Meals@users.noreply.github.com>
2021-02-03 11:10:07 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -21,13 +21,55 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            int n = (int)(Math.random() *(13)+1);
+            c.setValue(n);
+            int s = (int)(Math.random() * 4);
+            c.setSuit(Card.SUITS[s]);
+            magicHand[i] = c;
+            
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        
+        //        for (int i =0 ; i < magicHand.length; i++) {
+        //            	System.out.print(magicHand[i].getValue() + "\n");
+        //        	System.out.print(magicHand[i].getSuit() + "\n");
+        //        }
+        
+        Card luckyCard = new Card();
+        luckyCard.setValue(10);
+        luckyCard.setSuit("Clubs");
+        
+        boolean inHand = false;
+        
+        for (int i =0; i<magicHand.length; i++) {
+        	
+        	if (magicHand[i].getValue() == luckyCard.getValue()) {
+        		
+        		if (magicHand[i].getSuit().equals(luckyCard.getSuit())) {
+        			
+        			inHand = true;
+        		}
+        	}
+        }
+
+        
+        
+        if (inHand == true) {
+        	System.out.print("Found!");
+        }
+        else {
+        	System.out.print("Not Found");
+        }
+        
+        
+        
+        
+        
+        
     }
     
 }
